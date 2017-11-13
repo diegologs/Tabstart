91cc5d36525127cf203559c1246da32738fee71c
Frostqui <diego.lopezgarcia@hotmail.com>
2017-11-13 18:19:34 +0100

    Added background image from subreddit

--- js/index.js
+++ js/index.js
@@ -1,4 +1,11 @@
 $(document).ready(function() {
-  alert("sdajjdskadkjskjadksja");
+  
+  $.get( "https://www.reddit.com/r/wallpaper/top/.json?count=20?sort=new", function( json ) {
+    var image = json.data.children[0].data.url;
+    $( ".bg" ).css("background", "linear-gradient( rgba(13, 29, 36, 0.65), rgba(15, 19, 20, 0.45)), url("+ image +")");  
+    console.log( json.data.children[0].data.url );
+  });
+
+
 });
 
