d3dce14e79f95273ccf91ca3d9d2667a5b3b3788
Frostqui <diego.lopezgarcia@hotmail.com>
2018-04-09 15:27:19 +0200

    Minor changes

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+document.addEventListener('DOMContentLoaded',function(){
 
   startTime();
   getWallpaper();
@@ -19,35 +19,35 @@ var settings = false;
 
 var sites = default_sites.slice();
 
-var urlIconMap = jQuery.extend(true, {}, default_icons);
+var urlIconMap = deepExtend(urlIconMap, urlIconMap, default_icons); 
 
 String.prototype.getPureDomain = function () {
-  var temp = document.createElement("a")
-  temp.href = this
+  let temp = document.createElement('a');
+  temp.href = this;
 
-  var val = temp.host
-  if (val.indexOf("www") === 0) val = val.substring(val.indexOf("www.") + "www.".length)
+  let val = temp.host;
+  if (val.indexOf('www') === 0) val = val.substring(val.indexOf('www.') + 'www.'.length);
 
-  return val
+  return val;
 }
 
-var xhr = function (url, callback) {
-  var oReq = new XMLHttpRequest()
+let xhr = function (url, callback) {
+  let oReq = new XMLHttpRequest()
   oReq.onload = function () {
-    var response = this.responseText
-    callback(response)
+    let response = this.responseText;
+    callback(response);
   }
   oReq.onerror = function (e) {}
-  oReq.open("get", url, true)
-  oReq.send()
+  oReq.open('get', url, true);
+  oReq.send();
 }
 
 
 // Gets the sites stored in the localStorage
 
 function getStoredSites(callback) {
-  var stored_sites = JSON.parse(localStorage.getItem("sites"));
-  var stored_icons = JSON.parse(localStorage.getItem("icons"));
+  let stored_sites = JSON.parse(localStorage.getItem('sites'));
+  let stored_icons = JSON.parse(localStorage.getItem('icons'));
 
   if (stored_sites) {
     sites = stored_sites;
@@ -66,25 +66,24 @@ function getStoredSites(callback) {
 
 function add_site_button() {
 
-  $(".add_site").bind("click", function (e) {
-    var site = $("#add_site_input").val();
+  document.querySelector('.add_site').bind('click', function (e) {
+    let site = $('#add_site_input').val();
     addSite(site);
-
   })
 }
 
 
 function default_site_button() {
 
-  $(".default_settings").bind("click", function (e) {
-    console.log("Reseting to default settings....");
+  document.getElementByClassName('.default_settings').bind('click', function (e) {
+    console.log('Reseting to default settings....');
     sites = default_sites.slice();
-
-    urlIconMap = jQuery.extend(true, {}, default_icons);
+    //urlIconMap = jQuery.extend(true, {}, default_icons);
+    sites = default_sites.slice();
     refreshSites();
-    localStorage.setItem("sites", JSON.stringify(sites));
-    localStorage.setItem("icons", JSON.stringify(urlIconMap));
-    console.log("Default settings have been restored");
+    localStorage.setItem('sites', JSON.stringify(sites));
+    localStorage.setItem('icons', JSON.stringify(urlIconMap));
+    console.log('Default settings have been restored');
     delete_sitte_button();
   })
 }
@@ -92,10 +91,10 @@ function default_site_button() {
 
 function delete_sitte_button() {
 
-  var delete_button = document.getElementsByClassName("fa-trash");
+  let delete_button = document.getElementsByClassName('fa-trash');
   for (i = 0; i < delete_button.length; i++) {
     delete_button[i].onclick = function (e) {
-      var index = e.target.id;
+      let index = e.target.id;
       deleteSite(index);
       delete_sitte_button();
     }
@@ -104,14 +103,14 @@ function delete_sitte_button() {
 
 function settings_button() {
 
-  $(".settings_icon").bind("click", function (e) {
-    $(".addSite").toggle();
-    $("#overlay").toggle();
-    $(".settings_title").toggle();
-    $(".default_settings").toggle();
-    $(".fa-trash").toggle();
-    $(".author").toggle();
-    $(".github").toggle();
+  document.getElementByClassName('.settings_icon').bind('click', function (e) {
+    document.getElementByClassName$('.addSite').toggle();
+    document.getElementByClassName('#overlay').toggle();
+    document.getElementByClassName('.settings_title').toggle();
+    document.getElementByClassName('.default_settings').toggle();
+    document.getElementByClassName('.fa-trash').toggle();
+    document.getElementByClassName('.author').toggle();
+    document.getElementByClassName('.github').toggle();
 
     if (settings) {
       settings = false;
@@ -123,47 +122,47 @@ function settings_button() {
 }
 
 function startTime() {
-  var today = new Date();
-  var h = today.getHours();
-  var m = today.getMinutes();
-  var s = today.getSeconds();
+  let today = new Date();
+  let h = today.getHours();
+  let m = today.getMinutes();
+  let s = today.getSeconds();
   m = checkTime(m);
   s = checkTime(s);
   document.getElementById('clock').innerHTML =
-    h + ":" + m + ":" + s;
-  var t = setTimeout(startTime, 500);
+    h + ':' + m + ':' + s;
+  let t = setTimeout(startTime, 500);
 }
 
 function checkTime(i) {
   if (i < 10) {
-    i = "0" + i
+    i = '0' + i
   };
   return i;
 }
 
 function addSite(site) {
 
-  var icon_aux;
+  let icon_aux;
 
-  if (site.startsWith("http://")) {
+  if (site.startsWith('http://')) {
     site = site.slice(7);
   }
 
-  if (site.startsWith("https://")) {
+  if (site.startsWith('https://')) {
     site = site.slice(8);
   }
 
-  if (site.startsWith("www.")) {
+  if (site.startsWith('www.')) {
     site = site.slice(4);
   }
 
-  if (!site.startsWith("www.") && !site.startsWith("https://")) {
+  if (!site.startsWith('www.') && !site.startsWith('https://')) {
     site = 'https://www.' + site;
   }
 
 
   if (urlIconMap.hasOwnProperty(site)) {
-  
+
     icon_aux = urlIconMap[site]
 
   }
@@ -176,7 +175,7 @@ function addSite(site) {
 
 
       urlIconMap[site] = icon_aux;
-      localStorage.setItem("icons", JSON.stringify(urlIconMap));
+      localStorage.setItem('icons', JSON.stringify(urlIconMap));
       refreshSites();
 
     });
@@ -186,12 +185,12 @@ function addSite(site) {
   if (!icon_aux) {
 
     urlIconMap[site] = getIcon(site);
-    localStorage.setItem("icons", JSON.stringify(urlIconMap));
+    localStorage.setItem('icons', JSON.stringify(urlIconMap));
   }
 
   sites.push(site);
-  console.log("Added:  " + site);
-  localStorage.setItem("sites", JSON.stringify(sites));
+  console.log('Added:  ' + site);
+  localStorage.setItem('sites', JSON.stringify(sites));
   printSite(site);
   delete_sitte_button();
 
@@ -204,21 +203,21 @@ function deleteSite(index) {
     sites.splice(index, 1);
   }
 
-  localStorage.setItem("sites", JSON.stringify(sites));
-  localStorage.setItem("icons", JSON.stringify(urlIconMap));
+  localStorage.setItem('sites', JSON.stringify(sites));
+  localStorage.setItem('icons', JSON.stringify(urlIconMap));
   refreshSites();
 }
 
 function printSite(site) {
-  var site_icon;
-  var d = document;
-  var a;
-  var img;
-  var div;
-  var index;
-  var trash;
+  let site_icon;
+  let d = document;
+  let a;
+  let img;
+  let div;
+  let index;
+  let trash;
 
-  site_icon = "";
+  site_icon = '';
 
   if (urlIconMap.hasOwnProperty(site)) {
     site_icon = urlIconMap[site]
@@ -226,23 +225,23 @@ function printSite(site) {
 
   if (site_icon) {
 
-    if (!site_icon.startsWith("https://") && !site_icon.startsWith("http://") && !site_icon.startsWith("www.")) {
-      site_icon = "/img/" + site_icon + ".png";
+    if (!site_icon.startsWith('https://') && !site_icon.startsWith('http://') && !site_icon.startsWith('www.')) {
+      site_icon = '/img/' + site_icon + '.png';
     }
-    a = d.createElement("a");
-    div = d.createElement("div");
+    a = d.createElement('a');
+    div = d.createElement('div');
     a.href = site;
-    div.className = "col col-sm-2 site";
+    div.className = 'col col-sm-2 site';
 
-    img = d.createElement("img");
+    img = d.createElement('img');
     img.src = site_icon;
-    img.width = "110";
-    img.className = "site_img"
+    img.width = '110';
+    img.className = 'site_img'
 
-    trash = d.createElement("i");
-    trash.classList.add("fa");
+    trash = d.createElement('i');
+    trash.classList.add('fa');
 
-    trash.classList.add("fa-trash");
+    trash.classList.add('fa-trash');
 
     index = sites.indexOf(site);
     trash.setAttribute('id', index);
@@ -254,32 +253,33 @@ function printSite(site) {
       trash.style.display = 'none';
     }
 
-    trash.setAttribute("aria-hidden", "true");
+    trash.setAttribute('aria-hidden', 'true');
 
     a.appendChild(img);
     div.appendChild(a);
     div.appendChild(trash);
 
+    let most_visited = document.querySelector('.most_visited');
+    most_visited.appendChild(div);
 
-    $(".most_visited").append(div);
   } else {
 
     fetchIcon(site, function (icon) {
       site_icon = icon;
-      a = d.createElement("a");
-      div = d.createElement("div");
+      a = d.createElement('a');
+      div = d.createElement('div');
       a.href = site;
-      div.className = "col col-sm-2 site";
+      div.className = 'col col-sm-2 site';
 
-      img = d.createElement("img");
+      img = d.createElement('img');
       img.src = site_icon;
-      img.width = "110";
-      img.className = "site_img"
+      img.width = '110';
+      img.className = 'site_img'
 
-      trash = d.createElement("i");
-      trash.classList.add("fa");
+      trash = d.createElement('i');
+      trash.classList.add('fa');
 
-      trash.classList.add("fa-trash");
+      trash.classList.add('fa-trash');
 
       index = sites.indexOf(site);
       trash.setAttribute('id', index);
@@ -291,15 +291,16 @@ function printSite(site) {
 
         trash.style.display = 'none';
       }
-      trash.setAttribute("aria-hidden", "true");
+      trash.setAttribute('aria-hidden', 'true');
 
       a.appendChild(img);
       div.appendChild(a);
       div.appendChild(trash);
 
 
-      $(".most_visited").append(div);
+      let most_visited = document.querySelector('.most_visited');
 
+      most_visited.appendChild(div);
 
       delete_sitte_button();
 
@@ -318,36 +319,63 @@ function printSites() {
 
 function refreshSites() {
 
-  $(".most_visited").empty();
+  $('.most_visited').empty();
   printSites();
 
 
 }
 
+function deepExtend(out) {
+  out = out || {};
+
+  for (var i = 1; i < arguments.length; i++) {
+    var obj = arguments[i];
+
+    if (!obj)
+      continue;
+
+    for (var key in obj) {
+      if (obj.hasOwnProperty(key)) {
+        if (typeof obj[key] === 'object')
+          out[key] = deepExtend(out[key], obj[key]);
+        else
+          out[key] = obj[key];
+      }
+    }
+  }
+
+  return out;
+};
+
 function getIcon(site) {
-  return "https://logo.clearbit.com/" + site + "?s=200"
+  return 'https://logo.clearbit.com/' + site + '?s=200'
 }
 
+var image;
 
 function getWallpaper() {
 
-  $.get("https://www.reddit.com/r/wallpaper/top/.json?count=2?sort=new", function (json) {
-    var image = json.data.children[0].data.url;
-    $(".bg").css("background", "linear-gradient( rgba(0, 5, 30, 0.75), rgba(15, 19, 30, 0.25)), url(" + image + ")");
-    $(".bg").css("background-size", "cover");
-    $('.content').css('background', 'rgba(255, 255, 255, 0)');
 
-    // console.log(json.data.children[0].data.url);
+  fetch('https://www.reddit.com/r/wallpaper/top/.json?count=2?sort=new')
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(jsonResponse) {
+      image = jsonResponse;
+      console.log(image.data.children[0].data.url);
+      getComputedStyle(document.getElementsByClassName('.bg'))['background', 'linear-gradient( rgba(0, 5, 30, 0.75), rgba(15, 19, 30, 0.25)), url(' + image + ')'];
+      getComputedStyle(document.getElementsByClassName('.bg'))['background-size', 'cover'];
+      getComputedStyle(document.getElementsByClassName('.content'))['background', 'rgba(255, 255, 255, 0)'];
+
+    });
 
 
-
-  });
 }
 
 function getRandomColor() {
-  var letters = '0123456789ABCDEF';
-  var color = '#';
-  for (var i = 0; i < 6; i++) {
+  let letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
     color += letters[Math.floor(Math.random() * 16)];
   }
   return color;
@@ -364,7 +392,7 @@ function delete_top_site(url_to_delete) {
 
 
 function fetchIcon(url, _callback) {
-  var presetMatchedId
+  let presetMatchedId
   let parser;
   let doc;
 
@@ -378,28 +406,28 @@ function fetchIcon(url, _callback) {
   } else {
     xhr(url, function (r) {
       parser = new DOMParser()
-      doc = parser.parseFromString(r, "text/html")
+      doc = parser.parseFromString(r, 'text/html')
 
-      var icon = URI("favicon.ico").absoluteTo(url).toString()
-      var linkTags = doc.getElementsByTagName("link")
-      var icons = [].slice.call(linkTags).filter(function (tag) {
-        var attrRel = tag.getAttribute("rel")
-        return attrRel === "apple-touch-icon-precomposed" || attrRel === "apple-touch-icon" || attrRel === "shortcut icon" || attrRel === "icon"
+      let icon = URI('favicon.ico').absoluteTo(url).toString()
+      let linkTags = doc.getElementsByTagName('link')
+      let icons = [].slice.call(linkTags).filter(function (tag) {
+        let attrRel = tag.getAttribute('rel')
+        return attrRel === 'apple-touch-icon-precomposed' || attrRel === 'apple-touch-icon' || attrRel === 'shortcut icon' || attrRel === 'icon'
       })
 
-      var sizePreference = ["57x57", "60x60", "72x72", "76x76", "96x96", "114x114", "120x120", "144x144", "152x152", "180x180", "192x192"]
+      let sizePreference = ['57x57', '60x60', '72x72', '76x76', '96x96', '114x114', '120x120', '144x144', '152x152', '180x180', '192x192']
 
       icons.sort(function (a, b) {
-        var sizeA = a.getAttribute("sizes")
-        var sizeB = b.getAttribute("sizes")
+        let sizeA = a.getAttribute('sizes')
+        let sizeB = b.getAttribute('sizes')
         if (sizePreference.indexOf(sizeA) > sizePreference.indexOf(sizeB)) return -1
         if (sizePreference.indexOf(sizeA) < sizePreference.indexOf(sizeB)) return 1
         return 0
       })
 
       if (icons.length > 0) {
-        var iconElem = icons[0]
-        var iconHrefAttr = iconElem.getAttribute("href")
+        let iconElem = icons[0]
+        let iconHrefAttr = iconElem.getAttribute('href')
         icon = URI(iconHrefAttr).absoluteTo(url).toString()
       }
 
@@ -412,11 +440,12 @@ function fetchIcon(url, _callback) {
 
 
 String.prototype.getPureDomain = function () {
-  var temp = document.createElement("a")
+  let temp = document.createElement('a')
   temp.href = this
 
-  var val = temp.host
-  if (val.indexOf("www") === 0) val = val.substring(val.indexOf("www.") + "www.".length)
+  let val = temp.host
+  if (val.indexOf('www') === 0) val = val.substring(val.indexOf('www.') + 'www.'.length)
 
   return val
 }
+
